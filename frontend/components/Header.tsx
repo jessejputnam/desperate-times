a516a466014aa0d42b1e7d8c6b7b127767190511
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-01-19 16:28:26 -0500

    Add styled components to next compiler config; start styling header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,22 +3,51 @@ import styled from "styled-components";
 import Link from "next/link";
 import Nav from "./Nav";
 
-const Logo = styled.h1`
-  color: red;
+const LogoStyled = styled.h1`
+  background-color: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyled = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
 `;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <div className='bar'>
-        <Link href='/'>
-          <Logo>Desperate Times</Logo>
-        </Link>
+        <LogoStyled>
+          <Link href='/'>Desperate Times</Link>
+        </LogoStyled>
       </div>
+
       <div className='sub-bar'>
         <p>Search</p>
       </div>
+
       <Nav />
-    </header>
+    </HeaderStyled>
   );
 }
