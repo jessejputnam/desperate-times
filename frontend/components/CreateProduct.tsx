3dc7e2b10f5272f39b9b9b4f24ec5f8d67307485
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-02-03 15:36:40 -0500

    Finish xcreate product form

--- frontend/components/CreateProduct.tsx
+++ frontend/components/CreateProduct.tsx
@@ -3,11 +3,16 @@ import useForm from "@/lib/useForm";
 import Form from "./styles/Form";
 
 export default function CreateProduct() {
-  const { inputs, handleChange, clearForm, resetForm } = useForm({});
+  const { inputs, handleChange, clearForm, resetForm } = useForm();
+
+  function submit(e: any) {
+    e.preventDefault();
+    console.log(inputs);
+  }
 
   return (
     <div>
-      <Form>
+      <Form onSubmit={submit}>
         <fieldset>
           <label htmlFor='image'>
             Name
@@ -16,6 +21,7 @@ export default function CreateProduct() {
               id='image'
               name='image'
               onChange={handleChange}
+              required
             />
           </label>
           <label htmlFor='name'>
@@ -27,17 +33,30 @@ export default function CreateProduct() {
               placeholder='Name'
               value={inputs.name ?? ""}
               onChange={handleChange}
+              required
             />
           </label>
           <label htmlFor='price'>
             Price
             <input
-              type='Number'
+              type='number'
               id='price'
               name='price'
               placeholder='Price'
               value={inputs.price ?? ""}
               onChange={handleChange}
+              required
+            />
+          </label>
+          <label htmlFor='description'>
+            Description
+            <textarea
+              id='description'
+              name='description'
+              placeholder='Description'
+              value={inputs.description ?? ""}
+              onChange={handleChange}
+              required
             />
           </label>
           <button type='submit'>+ Add Product</button>
